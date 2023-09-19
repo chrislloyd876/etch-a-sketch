248b5d138c17f69802efd03acf1efae78d8038ce
chrislloyd876 <chrislloydlawrence@gmail.com>
2023-09-18 23:37:46 -0500

    Add random color changer for square
    Added function to change background color to a random color when the
    user hovers over any square.

--- javaScript.js
+++ javaScript.js
@@ -1,6 +1,16 @@
 const container =  document.querySelector("#container");
 
 
+function randomColor(){
+    let color1 = Math.floor(Math.random() * 256);
+    let color2 = Math.floor(Math.random() * 256);
+    let color3 = Math.floor(Math.random() * 256);
+
+    let rColor = "rgb(" + color1 + "," + color2 + "," + color3  + ")";
+    return rColor;
+}
+
+
 for(let rowNum = 0; rowNum < 16; rowNum++){
     const row = document.createElement("div");
     row.classList.add('row');
@@ -11,6 +21,10 @@ for(let rowNum = 0; rowNum < 16; rowNum++){
         column.classList.add('column');
         column.textContent = `${colNum + 1}`;
         row.appendChild(column);
+        column.onmouseover = function(){
+            let rCol = randomColor();
+            column.style.backgroundColor = rCol;
+        };
     }
 }
 
